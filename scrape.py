ffefa862acb4e643ad59fcb64fc4f8b6ae5905d1
Atanazy Gawrysiak <atanazy.gawrysiak@gmail.com>
2025-10-30 19:47:28 +0100

    fix scraping

--- scrape.py
+++ scrape.py
@@ -78,9 +78,9 @@ def add_rankings(df):
                         continue
                     ranking25 = row[0] if row[0] else ""
                     nazwa_szkoly = row[1] + row[2] if row[1] or row[2] else ""
-                    if nazwa_szkoly[0:2] != "LO":
+                    if nazwa_szkoly[0:2] == "LO" or "LO" not in nazwa_szkoly:
                         continue
-                    nazwa_szkoly = nazwa_szkoly.split("LO")[-1].strip()
+                    nazwa_szkoly = nazwa_szkoly.split("LO")[-2].strip() + " LO"
                     rankingi = [float(x) for x in row[4:7] if x != "-"] # places in 2024, 2023, 2022
                     srednia22do24 = round(sum(rankingi) / len(rankingi), 2) if rankingi else None
                     mask = df['nazwa_szkoly'].str.startswith(nazwa_szkoly)
